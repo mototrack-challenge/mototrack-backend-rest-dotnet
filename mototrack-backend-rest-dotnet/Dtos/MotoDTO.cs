cc3540fcb7fda71742c1fe5dd95173ffa4a01347
felipesora <felipeusora@gmail.com>
2025-09-09 14:08:25 -0300

    DTO e Mapper de Moto

---
+++ mototrack-backend-rest-dotnet/Dtos/MotoDTO.cs
@@ -0,0 +1,5 @@
+﻿using mototrack_backend_rest_dotnet.Models.Enums;
+
+namespace mototrack_backend_rest_dotnet.Dtos;
+
+public record MotoDTO(string Placa, string Chassi, ModeloMoto Modelo, StatusMoto Status);
---
+++ mototrack-backend-rest-dotnet/Mappers/MotoMapper.cs
@@ -0,0 +1,18 @@
+﻿using mototrack_backend_rest_dotnet.Dtos;
+using mototrack_backend_rest_dotnet.Models;
+
+namespace mototrack_backend_rest_dotnet.Mappers;
+
+public static class MotoMapper
+{
+    public static MotoEntity ToMotoEntity(this MotoDTO dto)
+    {
+        return new MotoEntity
+        {
+            Placa = dto.Placa,
+            Chassi = dto.Chassi,
+            Modelo = dto.Modelo,
+            Status = dto.Status
+        };
+    }
+}
