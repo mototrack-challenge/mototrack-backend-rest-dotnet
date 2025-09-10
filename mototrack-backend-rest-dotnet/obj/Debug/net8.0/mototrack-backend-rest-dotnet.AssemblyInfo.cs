26c8f40189c1a003d16abbe055dbadf5613bd6ff
felipesora <felipeusora@gmail.com>
2025-09-10 18:58:18 -0300

    Ajustando retorno do enum de Modelo

--- mototrack-backend-rest-dotnet/obj/Debug/net8.0/mototrack-backend-rest-dotnet.AssemblyInfo.cs
+++ mototrack-backend-rest-dotnet/obj/Debug/net8.0/mototrack-backend-rest-dotnet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mototrack-backend-rest-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c22aa8a4d8c4db376160928fb325ebbfaca63db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da0693ee25ae7db1cc63bb71badaee62352c327c")]
 [assembly: System.Reflection.AssemblyProductAttribute("mototrack-backend-rest-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mototrack-backend-rest-dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
