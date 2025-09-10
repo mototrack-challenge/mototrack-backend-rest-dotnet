0db8f22010beaf6fe0f1f5d503606acb8a274855
felipesora <felipeusora@gmail.com>
2025-09-10 15:22:06 -0300

    Service de Serviço

---
+++ mototrack-backend-rest-dotnet/Services/Interface/IServicoService.cs
@@ -0,0 +1,13 @@
+﻿using mototrack_backend_rest_dotnet.Dtos;
+using mototrack_backend_rest_dotnet.Models;
+
+namespace mototrack_backend_rest_dotnet.Services.Interface;
+
+public interface IServicoService
+{
+    Task<PageResultModel<IEnumerable<ServicoEntity>>> ObterTodosServicosAsync(int deslocamento = 0, int registrosRetornados = 10);
+    Task<ServicoEntity?> ObterServicoPorIdAsync(long id);
+    Task<ServicoEntity?> AdicionarServicoAsync(ServicoDTO servicoDTO);
+    Task<ServicoEntity?> EditarServicoAsync(long id, ServicoDTO novoServicoDTO);
+    Task<ServicoEntity?> DeletarServicoAsync(long id);
+}
--- mototrack-backend-rest-dotnet/obj/Debug/net8.0/mototrack-backend-rest-dotnet.AssemblyInfo.cs
+++ mototrack-backend-rest-dotnet/obj/Debug/net8.0/mototrack-backend-rest-dotnet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mototrack-backend-rest-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2bc30cd33011aa0999c15ffdec4a9b2bb3079b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29a592fc9933b229f301d0f49071ff89194479cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("mototrack-backend-rest-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mototrack-backend-rest-dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
