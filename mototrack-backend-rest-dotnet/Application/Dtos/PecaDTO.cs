04d73e819152fee214512404ebd66c54d555d42d
felipesora <felipeusora@gmail.com>
2025-09-16 21:35:44 -0300

    DTO e Mapper de Peça

---
+++ mototrack-backend-rest-dotnet/Application/Dtos/PecaDTO.cs
@@ -0,0 +1,3 @@
+﻿namespace mototrack_backend_rest_dotnet.Application.Dtos;
+
+public record PecaDTO(string Nome, string Codigo, string Descricao, int QuantidadeEstoque);
---
+++ mototrack-backend-rest-dotnet/Application/Mappers/PecaMapper.cs
@@ -0,0 +1,18 @@
+﻿using mototrack_backend_rest_dotnet.Application.Dtos;
+using mototrack_backend_rest_dotnet.Domain.Entities;
+
+namespace mototrack_backend_rest_dotnet.Application.Mappers;
+
+public static class PecaMapper
+{
+    public static PecaEntity ToColaboradorEntity(this PecaDTO dto)
+    {
+        return new PecaEntity
+        {
+            Nome = dto.Nome,
+            Codigo = dto.Codigo,
+            Descricao = dto.Descricao,
+            QuantidadeEstoque = dto.QuantidadeEstoque
+        };
+    }
+}
